d04ed1fc80edb9f02557976e15ff331cc434689b
Shweta <shwetan_techsys06@gmail.com>
2026-02-02 09:56:01 +0545

    change in login dto

--- inventory/DTOs/LoginDto.cs
+++ inventory/DTOs/LoginDto.cs
@@ -6,10 +6,3 @@
         public required string Upass { get; set; }
     }
 }
-
-
-
-//{
-//    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhbWFuIiwianRpIjoiMzMxMzMwMjQtM2RhNS00ZTBmLTk4ZjMtZjI3Yzc5ZThlNDVlIiwidWlkIjoiMTIwIiwiZXhwIjoxNzY5NDk0ODA4LCJpc3MiOiJpbnZlbnRvcnlBUEkiLCJhdWQiOiJpbnZlbnRvcnlVc2VycyJ9.FfjAs5NY2slqO3SxHdixjqIJysjAPSy4AP5JPJrIh7g",
-//  "username": "aman"
-//}
